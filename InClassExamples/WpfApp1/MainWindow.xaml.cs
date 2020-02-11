bd30220b4b0c99faec865bca3b78c0f2fd025c35
Clinton Carter <clinton.carter2018@ou.edu>
2020-02-11 13:13:30 -0600

    RICK AND MORTY

---
+++ InClassExamples/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string apiURL = "https://rickandmortyapi.com/api/character/";
+            RickAndMortyAPIResult apiInfo;
+            //get this next portion on exam but know how to use it
+            using (var client = new HttpClient())
+            {//async can break up a task into multiple parts and everyone can work on it at the same time
+                string json = client.GetStringAsync(apiURL).Result;
+
+                 apiInfo = JsonConvert.DeserializeObject<RickAndMortyAPIResult>(json);
+
+
+            }
+            foreach (var character in apiInfo.results)
+            {
+                LSTCharacters.Items.Add(character);
+            }
+            
+
+        }
+
+        private void LSTCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ResultObject selectedCharacter = (ResultObject)LSTCharacters.SelectedItem;
+            imgCharacter.Source = new BitmapImage(new System.Uri(selectedCharacter.image)); //get this on exame
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+        //    ResultObject selectedCharacter = (ResultObject)LSTCharacters.SelectedItem;
+        //    wbCharacterInfo.Source = new System.Uri(selectedCharacter.image);
+        //}
+    }
+}
---
+++ InClassExamples/WpfApp1/RickAndMortyAPIResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class RickAndMortyAPIResult
+    {
+        // know these are objects from json converter and whats inside of it
+        public InfoObject info{ get; set; }
+        public List<ResultObject> results { get; set; }
+    }
+    
+    public class ResultObject
+    {// has roughly 12 properties but only using 6
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+        public override string ToString()
+        {
+            return $"({id}) {name}";
+        }
+
+    }
+
+    public class InfoObject
+    { // get this from the json converter. has 4 properties
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+    }
+}
