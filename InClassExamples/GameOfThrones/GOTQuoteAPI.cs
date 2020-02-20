4a404a0e73869d899cc3f76e741a7cac8f86e990
Clinton Carter <clinton.carter2018@ou.edu>
2020-02-20 13:12:39 -0600

    got particapation

---
+++ InClassExamples/GameOfThrones/GOTQuoteAPI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThrones
+{
+    class GOTQuoteAPI
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+            
+        }
+    }
+}
---
+++ InClassExamples/GameOfThrones/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfThrones
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+         
+            
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://got-quotes.herokuapp.com/quotes";
+            GOTQuoteAPI gotQuotes;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                gotQuotes = JsonConvert.DeserializeObject<GOTQuoteAPI>(json);
+
+            }
+
+            txtQuote.Text = gotQuotes.quote;
+            txtName.Text = gotQuotes.character;
+        }
+    }
+}
